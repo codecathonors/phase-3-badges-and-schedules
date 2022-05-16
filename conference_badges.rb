72c4c464cf45f45ecae1b7e21382f1ad83e92e27
CatherineHonors <cathonors@gmail.com>
2022-05-16 19:56:41 -0400

    commit tree ce348b38c6ba96e9512e7f667f9694ee35211ce3

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator names
+    names.map { |name| badge_maker(name)}
+end
+
+def assign_rooms speakers
+    speakers.each_with_index { |speaker, index| "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"}
+end
